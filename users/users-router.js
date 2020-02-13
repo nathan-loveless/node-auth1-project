283db4d4555109358237a1c12d8b168ca08269f7
Nathan Loveless <nathansl2003@yahoo.com>
2020-02-12 23:43:29 -0500

    Almost done with MVP

---
+++ users/users-router.js
@@ -0,0 +1,31 @@
+const router = require('express').Router();
+const bcrypt = require('bcryptjs');
+const Users = require('./users-model.js');
+
+router.get('/', auth, (req, res) => {
+  Users.find()
+    .then(users => {
+      res.json(users);
+    })
+    .catch(err => res.send(err));
+});
+
+function auth(req, res, next) {
+	const {username, password} = req.headers;
+	console.log(username);
+	Users.findBy({ username })
+	    .first()
+	    .then(user => {
+	      if (user && bcrypt.compareSync(password, user.password)) {
+	      	console.log("Success!")
+	        next();
+	      } else {
+	        res.status(401).json({ message: 'Invalid Credentials' });
+	      }
+	    })
+	    .catch(error => {
+	      res.status(500).json(error);
+	    });
+}
+
+module.exports = router;
